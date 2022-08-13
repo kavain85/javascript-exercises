f14013f94e62744438ec0fa9e31567dca1807c75
kavain85 <gruye2@gmail.com>
2022-08-12 21:31:06 -0700

    Completed exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word,number) {
+    if (number < 0) return 'ERROR';
+    let connector = '';
+    for (i = 0; i < number; i++) {
+        connector += word;
+    } return connector;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let c = (f-32)*(5/9) // F to C conversion formula
+  if (c === 0) return c; // If value is 0, no decimal rounding
+  return +c.toFixed(1); // Decimal rounding to 1 decimal point
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let f = c*(9/5)+32; // C to F conversion formula
+  if (f === 0) return f; // If value is 0, no decimal rounding
+  return +f.toFixed(1); // Decimal rounding to 1 decimal point
 
 };
 
